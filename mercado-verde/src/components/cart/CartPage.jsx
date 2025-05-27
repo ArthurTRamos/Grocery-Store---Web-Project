42ca4a15395a6202ff9f61c2d537dac4518cdb5b
HenriqueDrago <dragohenrique21@gmail.com>
2025-05-27 11:14:06 -0300

    update cart and coupon components, add custom alert

--- mercado-verde/src/components/cart/CartPage.jsx
+++ mercado-verde/src/components/cart/CartPage.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import CardSelection from "./CardSelection";
 import CartItem from "./CartItem";
 import CouponSelection from "./CouponSelection";
+import CustomAlert from "../utility_elements/customAlert";
 
 import "./CartPage.css";
 
@@ -25,6 +26,8 @@ function CartPage({
   const [cardError, setCardError] = useState("");
   const [emptyCartError, setEmptyCartError] = useState("");
   const [avaliableBuy, setAvaliableBuy] = useState(true);
+  //alert
+  const [showSuccessModal, setShowSuccessModal] = useState(false);
 
   const navigate = useNavigate();
 
@@ -77,7 +80,7 @@ function CartPage({
         }
       });
       setAvaliableBuy(isAvailable);
-    }
+    };
 
     calculateSubtotalDiscount();
     checkStockAvailability();
@@ -85,7 +88,7 @@ function CartPage({
   }, [cartData, coupons, productData, selectedCoupon, userCoupons]);
 
   const handleFinish = () => {
-    if (!total) {
+    if (!subtotal) {
       setEmptyCartError(
         "Por favor, adicione ao menos um item antes de finalizar a compra!"
       );
@@ -101,7 +104,7 @@ function CartPage({
     }
     setCardError(""); // Clear error when card is selected
 
-    if(!avaliableBuy) {
+    if (!avaliableBuy) {
       setEmptyCartError("Alguns produtos não possuem estoque suficiente!");
       return;
     }
@@ -120,7 +123,7 @@ function CartPage({
 
     setProductData(updatedProductData);
     setCartData([]);
-    alert("Compra finalizada com sucesso!");
+    setShowSuccessModal(true);
 
     const selectedCouponUserData = userCoupons.find(
       (coupon) => coupon.couponNumber === selectedCoupon
@@ -132,109 +135,137 @@ function CartPage({
 
     setSelectedCoupon("");
     setSelectedCard("");
-
-    // Redirect to home page after purchase
-    navigate("/");
   };
 
-  const handleRedirect = () => {
+  const handleCloseSuccessModal = () => {
+    setShowSuccessModal(false);
+    setSelectedCoupon(""); // Clear coupon after confirmation
+    setSelectedCard(""); // Clear card after confirmation
+    navigate("/"); // Navigate after closing the modal
+  };
+
+  const handleAuthRedirect = () => {
     navigate("/auth");
-  }
+  };
+  const handleHomeRedirect = () => {
+    navigate("/");
+  };
+  const handleRecipeRedirect = () => {
+    navigate("/recipe");
+  };
 
   return (
     <div className="cart-page-container">
-      <div className="cart-page-left-container">
-        <div className="left-header">
-          <h1>Itens do Carrinho</h1>
-        </div>
-        <div className="cart-items-list">
-          {cartData.map((cartItem) => {
-            const product = productData.find(
-              (product) => product.id === cartItem.id
-            );
-            return (
-              <CartItem
-                key={cartItem.id}
-                cartItemData={{
-                  ...product,
-                  amount: cartItem.amount,
+      {cartData && cartData.length > 0 ? (
+        <>
+          <div className="cart-page-left-container">
+            {" "}
+            <div className="left-header">
+              <h1>Itens do Carrinho</h1>
+            </div>
+            <div className="cart-items-list">
+              {cartData.map((cartItem) => {
+                const product = productData.find(
+                  (product) => product.id === cartItem.id
+                );
+                return (
+                  <CartItem
+                    key={cartItem.id}
+                    cartItemData={{
+                      ...product,
+                      amount: cartItem.amount,
+                    }}
+                    changeAmount={(id, newAmount) => {
+                      // prev = previous state of the cartData
+                      setCartData((prev) =>
+                        // Iterates through the previous state and compared id
+                        // If the id matches, it updates the amount
+                        prev.map((item) =>
+                          // ...item = other properties of the item
+                          // amount: newAmount = replaces the amount property
+                          // else it keeps the item unchanged
+                          item.id === id ? { ...item, amount: newAmount } : item
+                        )
+                      );
+                    }}
+                    removeItem={(id) => {
+                      // filter only lets through itens with an id different from the one passed
+                      setCartData((prev) =>
+                        prev.filter((item) => item.id !== id)
+                      );
+                    }}
+                  />
+                );
+              })}
+            </div>{" "}
+          </div>
+          <div className="cart-page-right-container">
+            <div className="cart-summary">
+              <h3>Resumo do pedido</h3>
+              <div className="cart-summary-subtotal">
+                <p>Subtotal</p>
+                <p>R$ {subtotal.toFixed(2)}</p>
+              </div>
+              <div className="cart-summary-discount">
+                <p>Desconto</p>
+                <p>R$ {discount.toFixed(2)}</p>
+              </div>
+              <div className="cart-summary-total">
+                <p>Total</p>
+                <p>R$ {total.toFixed(2)}</p>
+              </div>
+            </div>
+            {paymentMethods ? (
+              <CardSelection
+                paymentMethods={paymentMethods}
+                onCardSelect={(card) => {
+                  setSelectedCard(card);
+                  setCardError(""); // Clear error when card is selected
                 }}
-                changeAmount={(id, newAmount) => {
-                  // prev = previous state of the cartData
-                  setCartData((prev) =>
-                    // Iterates through the previous state and compared id
-                    // If the id matches, it updates the amount
-                    prev.map((item) =>
-                      // ...item = other properties of the item
-                      // amount: newAmount = replaces the amount property
-                      // else it keeps the item unchanged
-                      item.id === id ? { ...item, amount: newAmount } : item
-                    )
-                  );
-                }}
-                removeItem={(id) => {
-                  // filter only lets through itens with an id different from the one passed
-                  setCartData((prev) => prev.filter((item) => item.id !== id));
-                }}
+                cardError={cardError}
               />
-            );
-          })}
-        </div>
-      </div>
-      <div className="cart-page-right-container">
-        <div className="cart-summary">
-          <h3>Resumo do pedido</h3>
-          <div className="cart-summary-subtotal">
-            <p>Subtotal</p>
-            <p>R$ {subtotal.toFixed(2)}</p>
-          </div>
-          <div className="cart-summary-discount">
-            <p>Desconto</p>
-            <p>R$ {discount.toFixed(2)}</p>
-          </div>
-          <div className="cart-summary-total">
-            <p>Total</p>
-            <p>R$ {total.toFixed(2)}</p>
-          </div>
-        </div>
-        {paymentMethods ? (
-          <CardSelection
-            paymentMethods={paymentMethods}
-            onCardSelect={(card) => {
-              setSelectedCard(card);
-              setCardError(""); // Clear error when card is selected
-            }}
-            cardError={cardError}
-          />
-        ) : null}
+            ) : null}
 
-        {userCoupons ? (
-          <CouponSelection
-            coupons={userCoupons}
-            onCouponSelect={setSelectedCoupon}
-          />
-        ) : null}
-        {userCoupons && paymentMethods ? (
-          <div className="cart-page-button">
-            <button
-              onClick={handleFinish}
-            >
-              Finalizar Compra
-            </button>
-            {emptyCartError && (
-              <p className="empty-cart-error">{emptyCartError}</p>
+            {userCoupons ? (
+              <CouponSelection
+                userCoupons={userCoupons}
+                coupons={coupons}
+                onCouponSelect={setSelectedCoupon}
+              />
+            ) : null}
+            {userCoupons && paymentMethods ? (
+              <div className="cart-page-button">
+                <button onClick={handleFinish}>Finalizar Compra</button>
+                {emptyCartError && (
+                  <p className="empty-cart-error">{emptyCartError}</p>
+                )}
+              </div>
+            ) : (
+              <div className="cart-page-button">
+                <button onClick={handleAuthRedirect}>
+                  Entre ou Cadastre-se para Finalizar a Compra
+                </button>
+              </div>
             )}
           </div>
-        ) : (
-          <div className="cart-page-button"> 
-          <button
-              onClick={handleRedirect}
-            >
-              Entre ou Cadastre-se para Finalizar a Compra
-            </button>
+          {showSuccessModal && (
+            <CustomAlert
+              messageHeader="Compra finalizada com sucesso!"
+              message=""
+              onClose={handleCloseSuccessModal}
+            />
+          )}
+        </>
+      ) : (
+        <>
+          <div className="no-items-cart-container">
+            <h1>Seu carrinho está vazio</h1>
+            <p>Adicione itens ao carrinho para continuar.</p>
+            <button onClick={handleHomeRedirect}>Voltar às Compras</button>
+            <button onClick={handleRecipeRedirect}>Ver Receitas</button>
           </div>
-        )}
-      </div>
+        </>
+      )}{" "}
     </div>
   );
 }
--- mercado-verde/src/components/cart/CouponSelection.jsx
+++ mercado-verde/src/components/cart/CouponSelection.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import "./CouponSelection.css";
 
-function CouponSelection({ onCouponSelect, coupons }) {
+function CouponSelection({ onCouponSelect, coupons, userCoupons }) {
   return (
     <div className="coupon-selection-container">
       <h3>Adicionar Cupom</h3>
@@ -12,17 +12,40 @@ function CouponSelection({ onCouponSelect, coupons }) {
         required
       >
         <option value="">Selecione um cupom</option>
-        {coupons
-          ? coupons.map((couponData) =>
-              couponData.used ? null : (
+        {userCoupons
+          ? userCoupons.map((couponData) => {
+              // First, check if the coupon is used
+              if (couponData.used) {
+                return null;
+              }
+
+              // Then, find the coupon details from the 'coupons' array
+              const foundCoupon = coupons.find(
+                (coupon) => coupon.couponNumber === couponData.couponNumber
+              );
+
+              // If the coupon details are not found in 'coupons', don't render the option
+              if (!foundCoupon) {
+                return null;
+              }
+
+              // If the coupon is not used AND its details are found, render the option
+              return (
                 <option
+                  className="coupon-option"
                   key={couponData.couponNumber}
                   value={couponData.couponNumber}
                 >
-                  <p>{couponData.couponNumber}</p>
+                  <p>{couponData.couponNumber} </p>
+                  <p>{"["}
+                    {foundCoupon.type === "money"
+                      ? `R$ ${foundCoupon.discount.toFixed(2)}`
+                      : `${foundCoupon.discount}%`}
+                    {"]"}
+                  </p>
                 </option>
-              )
-            )
+              );
+            })
           : null}
       </select>
     </div>
